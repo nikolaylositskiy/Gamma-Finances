8c1baae6ace94c67feb1f6c5b1371521069bf725
Nikolay Lositskiy <niklos2506@gmail.com>
2015-01-18 22:44:47 +0200

    fix hw15

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,8 +2,7 @@ class Transaction < ActiveRecord::Base
 	belongs_to :expense
 	
   validates :sum, numericality: { greater_than: 0 }
-  #validates :date, format: { with: /(19|20)\d\d-((0[1-9]|1[012])-(0[1-9]|[12]\d)|(0[13-9]|1[012])-30|(0[13578]|1[02])-31)/}
   validates :reason, presence: true
-  validates :traffic, numericality: { greater_than: 0, less_than: 2 }
+  validates :traffic, numericality: { greater_than: 0, less_than: 3 }
  
 end
